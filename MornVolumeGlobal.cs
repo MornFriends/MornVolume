84fe94fe9aef694e00acbbdd609232354056aec9
まっともぉん <pikapikan0526@gmail.com>
2025-02-23 15:05:54 +0900

    MornGlobalBase

--- MornVolumeGlobal.cs
+++ MornVolumeGlobal.cs
@@ -8,11 +8,6 @@ namespace MornVolume
     [CreateAssetMenu(fileName = nameof(MornVolumeGlobal), menuName = "Morn/" + nameof(MornVolumeGlobal))]
     internal class MornVolumeGlobal : MornGlobalBase<MornVolumeGlobal>
     {
-#if DISABLE_MORN_ASPECT_LOG
-        protected override bool ShowLog => false;
-#else
-        protected override bool ShowLog => true;
-#endif
         protected override string ModuleName => nameof(MornVolume);
         [SerializeField] private AudioMixer _mixer;
         [SerializeField] private List<string> _volumeKeys;
